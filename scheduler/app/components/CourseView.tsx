2da8701301617c597a86a36405913c422a5db826
Camila <clee@itba.edu.ar>
2025-01-04 17:26:09 -0300

    safari sticky

--- scheduler/app/components/CourseView.tsx
+++ scheduler/app/components/CourseView.tsx
@@ -168,7 +168,7 @@ const CourseView: React.FC<CourseViewProps> = ({
   </div>
 
   {selectedCourses.length > 0 && (
-    <div className="sticky bottom-0 left-0 right-0 z-40">
+    <div className="sticky bottom-0 left-0 right-0 z-40"   style={{ position: '-webkit-sticky' }}>
       <div className="max-w-7xl mx-auto">
         <div className="bg-background shadow-lg">
           <div className={`

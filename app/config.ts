03502ee07949cd09053b4a5646ad3494c7711e09
Uriel Sosa Vazquez <usosavazquez@itba.edu.ar>
2025-10-23 21:53:59 -0300

    use NEXT_PUBLIC_ for client side

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 const config = {
   api: {
-    baseUrl: process.env.CEITBA_API_URL,
+    baseUrl: process.env.NEXT_PUBLIC_CEITBA_API_URL,
     endpoints: {
       subjects: "/api/v1/subjects",
     },
